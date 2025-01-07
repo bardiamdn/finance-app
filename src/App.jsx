19c8643a822b48bbd05cb28d39537e581d7e8906
bardia <bardia7madani8@gmail.com>
2025-01-07 16:42:33 +0300

    add favicons and improve routing

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,46 @@ function App() {
   const { mainLoading } = useContext(ProfileDataContext);
   const [cautionDown, setCautionDown] = useState(true);
 
-
   if (mainLoading) {
     <div className="w-full h-full bg-white" />;
   }
   return (
     <>
-      <div className={`z-10 fixed top-0 left-0 w-full md:h-[60px] h-[100px] bg-red-500 ${cautionDown ? "" : "-translate-y-full"} transition-all duration-300`}>
-        <span className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-sm text-center">⚠️ <strong>Important Notice:</strong> This is a development/testing site and not a finalized product. Data entered here may be lost or deleted without notice. Please do not submit any sensitive or personal information.</span>
-        <button onClick={() => setCautionDown(false)} className="absolute top-1/2 right-0 transform -translate-x-full -translate-y-1/2">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
+      <div
+        className={`z-10 fixed top-0 left-0 w-full md:h-[60px] h-[70px] bg-red-500 ${
+          cautionDown ? "" : "-translate-y-full"
+        } transition-all duration-300`}
+      >
+        <span className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white md:text-sm text-xs text-center">
+          ⚠️ <strong>Important Notice:</strong> This is a development/testing
+          site and not a finalized product. Data entered here may be lost or
+          deleted without notice. Please do not submit any sensitive or personal
+          information.
+        </span>
+        <button
+          onClick={() => setCautionDown(false)}
+          className="absolute top-1/2 right-0 transform -translate-x-full -translate-y-1/2"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="black"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-6 h-6"
+          >
             <path d="M6 18L18 6M6 6l12 12"></path>
           </svg>
         </button>
-      </div >
-      <div className={`mainWrapper ${cautionDown ? "md:mt-[60px] mt-[100px]" : "mt-0"} transition-all duration-300`}>
+      </div>
+      <div
+        className={`mainWrapper ${
+          cautionDown ? "md:mt-[60px] mt-[70px]" : "mt-0"
+        } transition-all duration-300`}
+      >
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-
-
           <Header />
 
           <Routes>
